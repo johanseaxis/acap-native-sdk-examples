85ec9f23cd955d9fa74f625962c9e54a9b20582c
joha-axis <71760803+joha-axis@users.noreply.github.com>
2020-09-30 11:21:22 +0200

    Add docstrings and minor improvements (#6)
    * Add tensorflow to larod example v1
    
    * Add PEP8 conformance, simplifications to code, polishes to README
    
    * Fix incorrect log str, broken links, remove comments
    
    * add emph to important line
    
    * change labels used, remove unnecessary syslogs
    
    * remove acap cmdline install option
    
    * add mention of dtype byte allocation
    
    * fix incorrect output ordering, README polishes
    
    * change example path, add docstrings, refactorization of code, fix README issues
    
    * fix wording
    
    * merge changes from master

--- tensorflow-to-larod/env/convert_model.py
+++ tensorflow-to-larod/env/convert_model.py
@@ -1,3 +1,14 @@
+""" convert_model.py
+    Converts a saved Tensorflow SavedModel model to the Tensorflow Lite
+    format. The conversion is also set up to quantize the model to the INT8
+    precision, which allows usage on INT8 compatible devices.
+
+    Usage: python convert_model.py -i <SavedModel path> \
+        -d <dataset directory for data generation> [-o <.tflite output path>]
+"""
+
+# Some code adapted from https://www.tensorflow.org/lite/performance/post_training_quantization
+
 import argparse
 import glob
 import numpy as np
@@ -12,21 +23,30 @@ parser.add_argument('-i', '--input', type=str, required=True,
                     help='path to the SavedModel to convert')
 parser.add_argument('-d', '--dataset', type=str, required=True,
                     help='path to the dataset directory')
-parser.add_argument('-o', '--output', type=str, default='converted_model.tflite',
+parser.add_argument('-o', '--output', type=str,
+                    default='converted_model.tflite',
                     help='path to output the .tflite model to')
 
 args = parser.parse_args()
 
 
-# !! IMPORTANT !! You need to define this generator yourself to fit your model!
-# A data generator which produces samples from the model's domain
-# Calling yield on this generator should output samples of the same type and
-# shape as the inputs to the model
+# !! IMPORTANT !! You need to define this generator yourself if you are using
+# a model different from the one defined in the example!
 def a_representative_datagenerator():
-    # List the samples to yield
+    """ A data generator which produces samples from the model's domain.
+        Calling this generator should output samples of the same type
+        and shape as the inputs to the model, similar to those it has been
+        trained on.
+
+    Yields:
+        np.float32 array: An RGB image from the dataset directory, which has
+            been processed like the images the model has been
+            trained on. In this case, this includes
+            normalization and resizing. The output array has the
+            shape (1, 256, 256, 3).
+    """
     samples = glob.glob(os.path.join(args.dataset, '*'))
     for sample_path in samples:
-        # Load and preprocess the input data
         sample = Image.open(sample_path).resize((256, 256))
         if sample.mode != 'RGB':
             continue
@@ -35,8 +55,8 @@ def a_representative_datagenerator():
         yield [preprocessed_sample]
 
 
-# Create the converter. As we're converting a model of the
-# SavedModel format, we're using the from_saved_model function
+# Create the converter. As the model to convert is of the
+# SavedModel format, the from_saved_model function is used
 converter = tf.lite.TFLiteConverter.from_saved_model(args.input)
 
 # Flags which set what optimizations to perform. The DEFAULT flag
@@ -46,11 +66,10 @@ converter.optimizations = [tf.lite.Optimize.DEFAULT]
 # Set the converter to use the data generator defined above
 converter.representative_dataset = a_representative_datagenerator
 
-# Ensure that if any ops can't be quantized, the converter throws an error
+# Select the set of operators to use
 converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-converter.target_spec.supported_types = [tf.int8]
 
-# Set the input and output tensors to unsigned INT8
+# Set the desired datatype for inputs and outputs
 converter.inference_input_type = tf.uint8
 converter.inference_output_type = tf.uint8
 
